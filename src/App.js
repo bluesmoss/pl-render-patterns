f9a07fbbd6b4f5795d23c4acca139cf811617029
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-10-02 16:33:33 -0500

    chore: add main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 // import './App.css';
+import React from "react";
+import { TodoCounter } from './TodoCounter'
+import { TodoSearch } from './TodoSearch'
+import { TodoList } from './TodoList'
+import { TodoItem } from './TodoItem'
+import { CreateTodoButton } from './CreateTodoButton'
 
+
+
+const todos = [
+  { text: "Cortar cebolla" , completed: false},
+  { text: "Tomar curso" , completed: false},
+  { text: "Llorar con la llorona" , completed: false},
+  { text: "Lalala" , completed: false},
+
+
+]
 
 function App() {
   return (
-    
+    <React.Fragment>
+      <TodoCounter/>
+      <TodoSearch/>
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton/>      
+    </React.Fragment>
   );
 }
 
