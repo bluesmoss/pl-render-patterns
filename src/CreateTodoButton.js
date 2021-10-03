ee3e76e22b13145e016dfb1aef4284976a808d95
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-10-03 17:11:28 -0500

    chore(search): add state variables

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,20 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
+
+  const  onClickFunction = (msg) => {
+    alert(msg)
+  }
+
   return (
-    <button className="CreateTodoButton">+</button>
+    <button 
+      className="CreateTodoButton"
+      onClick={
+        () => onClickFunction('hola')
+      }
+    >
+      +
+      </button>
   );
 }
 
