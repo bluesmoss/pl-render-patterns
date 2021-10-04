be3f8f7e760b40bc3175c321506bd1e226f622cf
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2021-10-03 19:57:05 -0500

    chore: add local storage dat saving

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { AppUI } from './AppUI';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el cursso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'LALALALAA', completed: true },
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el cursso de intro a React', completed: false },
+//   { text: 'Llorar con la llorona', completed: false },
+//   { text: 'LALALALAA', completed: true },
+// ];
+
 
 function App() {
 
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1')
+  let parsedTodos;
+  
+  if(!localStorageTodos){
+    localStorage.setItem('TODOS_V1', JSON.stringify([]))
+    parsedTodos = []
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos)
+  }
+
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('')
   
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -30,18 +42,23 @@ function App() {
     
   }
 
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos))
+    setTodos(newTodos)
+  }
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text == text)
     const newTodos = [...todos]
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text == text)
     const newTodos = [...todos]
     newTodos.splice(todoIndex,1)
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }  
   
 
