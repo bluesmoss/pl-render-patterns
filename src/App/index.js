8e23de552cb73b3c7e0203f668e216bf8c6202be
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2022-03-14 16:12:45 -0600

    chore!: remove context and provider

--- src/App/index.js
+++ src/App/index.js
@@ -1,57 +1,67 @@
 import React, {useState} from 'react';
-import { AppUI } from './AppUI';
-import { TodoProvider } from '../TodoContext'
+import { useTodos } from './useTodos'
 import { TodoCounter } from '../components/TodoCounter';
-
-
-// function App(){
-
-//   const [state, setState] = useState('shared state')
-
-//   return <>
-//     <TodoHeader>
-//       <TodoCounter/>
-//       <TodoSearch/>
-//     </TodoHeader>
-//     <TodoList>
-//       <TodoItem state={state}/>
-//     </TodoList>
-//   </>
-// }
-
-// function TodoHeader({children}){
-//   return <header>
-//     {children}
-//   </header>
-// }
-
-// function TodoList({children}){
-//   return <section className='TodoList-container'>
-//     {children}
-//   </section>
-// }
-
-// function TodoCounter(){
-//   return <p>TodoCounter</p>
-// }
-
-// function TodoSearch(){
-//   return <p>TodoSearch</p>
-// }
-
-// function TodoItem({state}){
-// return <p>TodoItem state: {state}</p>
-// }
+import { TodoSearch } from '../components/TodoSearch';
+import { TodoList } from '../components/TodoList';
+import { TodoItem } from '../components/TodoItem';
+import { CreateTodoButton } from  '../components/CreateTodoButton'
+import { Modal } from '../components/Modal'
+import { TodoForm } from '../components/TodoForm'
+import { TodoHeader } from "../components/TodoHeader";
 
 
 function App() {
 
-  return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-    
-  );
+
+  const { 
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    totalTodos, 
+    completedTodos,
+    searchValue, 
+    setSearchValue,
+    addTodo} =useTodos()
+
+    return (
+      <React.Fragment>
+      <TodoHeader>
+        <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>      
+      </TodoHeader>
+
+      <TodoList>
+          {error && <p>Hubo un error....</p>}
+          {loading && <p>Loading....</p>}
+          {(!loading && !searchedTodos.length) && <p>Creat tu primer todo</p>}
+
+          {searchedTodos.map(todo => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={()=> deleteTodo(todo.text)}
+            />
+          ))}
+      </TodoList>
+
+      { !!openModal && (
+        <Modal>
+          <TodoForm addTodo={addTodo} setOpenModal={setOpenModal}/>
+        </Modal>
+      )}
+
+
+      <CreateTodoButton
+        setOpenModal = {setOpenModal}
+      />
+    </React.Fragment>
+  )
 }
 
 export default App;
--- src/App/useTodos.js
+++ src/App/useTodos.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {useLocalStorage} from './useLocalStorage'
 
-const TodoContext = React.createContext()
 
-function TodoProvider(props){
+
+function useTodos(){
 
     const {item: todos, saveItem:saveTodos, loading, error} = useLocalStorage('TODOS_V1', []);
 
@@ -51,8 +51,7 @@ function TodoProvider(props){
       
   
 
-    return (
-        <TodoContext.Provider value={{
+    return {
             totalTodos,
             completedTodos,
             searchValue,
@@ -65,11 +64,8 @@ function TodoProvider(props){
             openModal,
             setOpenModal,
             addTodo
-
-        }}>
-            {props.children}
-        </TodoContext.Provider>
-    )
+        }
+    
 }
 
-export {TodoContext, TodoProvider}
+export { useTodos}
