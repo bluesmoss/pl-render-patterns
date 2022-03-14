1f6ef994741664ca326747b5f50ab02be3ac0ce8
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2022-03-14 12:57:30 -0600

    chore(app): add component compistion example

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { AppUI } from './AppUI';
 import { TodoProvider } from '../TodoContext'
+//import { TodoCounter } from '../components/TodoCounter';
 
 
-function App() {
+function App(){
 
-  return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-    
-  );
+  const [state, setState] = useState('shared state')
+
+  return <>
+    <TodoHeader>
+      <TodoCounter/>
+      <TodoSearch/>
+    </TodoHeader>
+    <TodoList>
+      <TodoItem state={state}/>
+    </TodoList>
+  </>
 }
+
+function TodoHeader({children}){
+  return <header>
+    {children}
+  </header>
+}
+
+function TodoList({children}){
+  return <section className='TodoList-container'>
+    {children}
+  </section>
+}
+
+function TodoCounter(){
+  return <p>TodoCounter</p>
+}
+
+function TodoSearch(){
+  return <p>TodoSearch</p>
+}
+
+function TodoItem({state}){
+return <p>TodoItem state: {state}</p>
+}
+
+
+// function App() {
+
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+    
+//   );
+// }
 
 export default App;
